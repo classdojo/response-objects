74ec910abb6b647c21e4da8c927e6dbb6e9c96ae
Dom Bellizzi <dominick@classdojo.com>
2017-08-29 18:20:24 +0000

    add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+declare module "response-objects" {
+
+  namespace ResponseObjects {
+    let MARKER: any;
+
+    export interface BaseResponseObject {
+      body?: any;
+      status: number;
+      headers?: {
+        [index: string]: string;
+      };
+    }
+
+    interface ResponseObject extends BaseResponseObject {
+      toJSON(): BaseResponseObject;
+      toString(): string;
+    }
+
+    interface ErrorResponseObject extends ResponseObject, Error {
+    }
+
+    function setBodyCreator(fn: (code: number, body: string) => any): void;
+
+    function Ok(body: any | string, headers?: any): ResponseObject;
+
+    function OK(body: any | string, headers?: any): ResponseObject;
+
+    function NoContent(): ResponseObject;
+
+    function MethodNotAllowed(body: any, headers: any): ErrorResponseObject;
+
+    function Unauthorized(): ErrorResponseObject;
+
+    function Forbidden(): ErrorResponseObject;
+
+    function BadRequest(message?: string): ErrorResponseObject;
+
+    function NotFound(url: string): ErrorResponseObject;
+
+    function InternalServerError(err: Error): ErrorResponseObject;
+  }
+  function ResponseObjects(status: number, body: any, headers?: {[index: string]: string; }): ResponseObjects.ResponseObject;
+
+  export = ResponseObjects;
+}
